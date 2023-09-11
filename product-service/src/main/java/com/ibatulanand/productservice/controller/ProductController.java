e4dbfc8d53c69e3e357d587c01193a713b9d0f1c
Atul Anand <ib.atulanandjp@gmail.com>
2023-09-12 00:20:55 +0900

    Completed product-service implementation

---
+++ product-service/src/main/java/com/ibatulanand/productservice/controller/ProductController.java
@@ -0,0 +1,30 @@
+package com.ibatulanand.productservice.controller;
+
+import com.ibatulanand.productservice.dto.ProductRequest;
+import com.ibatulanand.productservice.dto.ProductResponse;
+import com.ibatulanand.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest) {
+        productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
+    }
+}
